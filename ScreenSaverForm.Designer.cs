8ed7bbd2fa1fa86f5adc53eff973963c1faddd8a
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2023-12-12 17:35:18 -0500

    Initial commit

---
+++ ScreenSaverForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿using AxWMPLib;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace Bender_Saver
+{
+    partial class ScreensaverForm : Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreensaverForm));
+            axWindowsMediaPlayer1 = new AxWindowsMediaPlayer();
+            moveTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).BeginInit();
+            SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            axWindowsMediaPlayer1.Enabled = true;
+            axWindowsMediaPlayer1.Location = new Point(197, 38);
+            axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            axWindowsMediaPlayer1.OcxState = (AxHost.State)resources.GetObject("axWindowsMediaPlayer1.OcxState");
+            axWindowsMediaPlayer1.Size = new Size(940, 960);
+            axWindowsMediaPlayer1.TabIndex = 0;
+            axWindowsMediaPlayer1.TabStop = false;
+            // 
+            // moveTimer
+            // 
+            moveTimer.Tick += moveTimer_Tick;
+            // 
+            // ScreensaverForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.Black;
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(axWindowsMediaPlayer1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ScreensaverForm";
+            StartPosition = FormStartPosition.Manual;
+            Text = "Form1";
+            Load += ScreensaverForm_Load;
+            KeyPress += ScreensaverForm_KeyPress;
+            MouseClick += ScreensaverForm_MouseClick;
+            MouseMove += ScreensaverForm_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer moveTimer;
+    }
+}
---
+++ ScreenSaverForm.cs
@@ -0,0 +1,111 @@
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Bender_Saver
+{
+    public partial class ScreensaverForm
+    {
+        private Point mouseLocation;
+        private bool previewMode = false;
+        private const int BOUNCE_SPEED = 1;
+        private const int BOUNCE_LIMIT = 10;
+        private int _xVelocity = BOUNCE_SPEED;
+        private int _yVelocity = BOUNCE_SPEED;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern bool GetClientRect(IntPtr hWnd, out Rectangle lpRect);
+
+        public ScreensaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            this.Size = new Size(Bounds.Width, Bounds.Height);
+        }
+
+        public ScreensaverForm(IntPtr PreviewWndHandle)
+        {
+            InitializeComponent();
+
+            // Set the preview window as the parent of this window
+            SetParent(this.Handle, PreviewWndHandle);
+
+            // Make this a child window so it will close when the parent dialog closes
+            // GWL_STYLE = -16, WS_CHILD = 0x40000000
+            SetWindowLong(this.Handle, -16, new IntPtr(GetWindowLong(this.Handle, -16) | 0x40000000));
+
+            // Place our window inside the parent
+            Rectangle ParentRect;
+            GetClientRect(PreviewWndHandle, out ParentRect);
+            Size = ParentRect.Size;
+            Location = new Point(0, 0);
+            previewMode = true;
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ScreensaverForm_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = @"C:\bender.mp4";
+            axWindowsMediaPlayer1.stretchToFit = true;
+            Cursor.Hide();
+            TopMost = true;
+            moveTimer.Interval = 30;
+            moveTimer.Tick += new EventHandler(moveTimer_Tick);
+            moveTimer.Start();
+        }
+
+        private void ScreensaverForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+        }
+
+        private void ScreensaverForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreensaverForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void moveTimer_Tick(object sender, EventArgs e)
+        {
+            Rectangle pictureRect = axWindowsMediaPlayer1.Bounds;
+
+            int newX = axWindowsMediaPlayer1.Location.X + _xVelocity;
+            int newY = axWindowsMediaPlayer1.Location.Y + _yVelocity;
+
+            if (newX <= 0 || newX + pictureRect.Width >= Size.Width)
+            {
+                _xVelocity *= -1;
+            }
+            if (newY <= 0 || newY + pictureRect.Height >= Size.Height)
+            {
+                _yVelocity *= -1;
+            }
+
+            axWindowsMediaPlayer1.Location = new Point(newX, newY);
+        }
+    }
+}
